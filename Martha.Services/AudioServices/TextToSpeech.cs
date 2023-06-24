05b7efd1ff8e25119e409760344374f9ddee48af
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-25 00:42:31 +0200

    Added new TextToSpeech service for Windows platform

---
+++ Martha.Services/AudioServices/TextToSpeech.cs
@@ -0,0 +1,54 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace Martha.Services.AudioServices
+{
+    using System.Speech.Synthesis;
+
+    public class TextToSpeech
+    {
+        #region Fields
+        private readonly SpeechSynthesizer _speechSynthesizer;
+        #endregion
+
+        #region Construction
+        public TextToSpeech()
+        {
+            _speechSynthesizer = new SpeechSynthesizer();
+        }
+        #endregion
+
+        #region Pronouncation Methods
+        public void Speak(string text)
+        {
+            _speechSynthesizer.SpeakAsync(text);
+        }
+        #endregion
+
+        #region Cancellation Methods
+        public void Dispose()
+        {
+            _speechSynthesizer.Pause();
+            _speechSynthesizer.Dispose();
+        }
+        public void StopSpeech()
+        {
+            _speechSynthesizer.SpeakAsyncCancelAll();
+        }
+        #endregion
+
+        #region SpeechSynthesizer Properties
+        public int SpeechVolume
+        {
+            get { return _speechSynthesizer.Volume; }
+            set { _speechSynthesizer.Volume = value; }
+        }
+        public int SpeechRate
+        {
+            get { return _speechSynthesizer.Rate; }
+            set { _speechSynthesizer.Rate = value; }
+        }
+        #endregion
+    }
+
+}
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
--- Martha.Services/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Martha.Services
-{
-    public class Class1
-    {
-
-    }
-}
