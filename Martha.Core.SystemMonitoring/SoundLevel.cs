c5b6a6748b1005c5929689c523a1161c2171ecbd
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-30 22:29:50 +0200

    Added SystemMonitoring section

---
+++ Martha.Core.SystemMonitoring/SoundLevel.cs
@@ -0,0 +1,28 @@
+﻿namespace Martha.Core.SystemMonitoring
+{
+    using NAudio.CoreAudioApi;
+    public static class SoundLevel
+    {
+        public static float GetMasterVolumeLevel()
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                var audioEndpointVolume = defaultDevice.AudioEndpointVolume;
+                var masterVolumeLevel = audioEndpointVolume.MasterVolumeLevelScalar;
+                return masterVolumeLevel;
+            }
+        }
+
+        public static void SetMasterVolumeLevel(float level)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                var audioEndpointVolume = defaultDevice.AudioEndpointVolume;
+                audioEndpointVolume.MasterVolumeLevelScalar = level;
+            }
+        }
+
+    }
+}
