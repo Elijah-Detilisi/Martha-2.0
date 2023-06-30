e9a37f89935e7c6ad20cc36897cd91f5aa1a8a79
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-07-01 01:43:38 +0200

    Create MP3Player.cs

---
+++ Martha.Core.Media/MP3Player.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Martha.Core.Media
+{
+    using NAudio.Wave;
+    public class MP3Player
+    {
+        #region Fields
+        private WaveOutEvent _outputDevice;
+        #endregion
+
+        #region Construction
+        public MP3Player()
+        {
+            _outputDevice = new WaveOutEvent();
+        }
+        #endregion
+
+        #region Media controllers
+        public void Play(string mp3File)
+        {
+            using (var audioFile = new AudioFileReader(mp3File))
+            {
+                this.Stop();
+                _outputDevice.Init(audioFile);
+                _outputDevice.Play();
+            }
+        }
+        public void Stop()
+        {
+            if (_outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                _outputDevice.Stop();
+            }
+        }
+        public void Pause()
+        {
+            if (_outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                _outputDevice.Pause();
+            }
+        }
+        #endregion
+    }
+}
