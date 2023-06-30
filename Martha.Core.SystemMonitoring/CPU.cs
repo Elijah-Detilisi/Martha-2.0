44cdc5aabff29eae8574d498aaa6c7dc57604e25
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-30 23:44:39 +0200

    Create CPU.cs

---
+++ Martha.Core.SystemMonitoring/CPU.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CA1416 // Validate platform compatibility
+namespace Martha.Core.SystemMonitoring
+{
+    using System.Diagnostics;
+    public class CPU
+    {
+        public float GetCurrentCPUUsage()
+        {
+            using (var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total"))
+            {
+                float cpuUsage = cpuCounter.NextValue();
+                return cpuUsage;
+            }
+        }
+    }
+}
+#pragma warning restore CA1416 // Validate platform compatibility
