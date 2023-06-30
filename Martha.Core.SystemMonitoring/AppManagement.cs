7203971940ab66515ca746716f9b316e7622cf55
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-07-01 00:06:08 +0200

    Create AppManagement.cs

---
+++ Martha.Core.SystemMonitoring/AppManagement.cs
@@ -0,0 +1,23 @@
+﻿namespace Martha.Core.SystemMonitoring
+{
+    using System.Diagnostics;
+
+    public static class AppManagement
+    {
+        public static void OpenApplication(string applicationPath)
+        {
+            Process.Start(applicationPath);
+        }
+
+        public static void CloseApplication(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            foreach (Process process in processes)
+            {
+                process.CloseMainWindow();
+                process.WaitForExit();
+            }
+        }
+
+    }
+}
