c7f429719e266989903177468bac8e9bab253bb3
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-30 23:31:36 +0200

    Create HardDrive.cs

---
+++ Martha.Core.SystemMonitoring/HardDrive.cs
@@ -0,0 +1,66 @@
+﻿namespace Martha.Core.SystemMonitoring
+{
+    using System.IO;
+
+    public class HardDrive
+    {
+        #region Fields
+        private readonly DriveInfo _driveInfo;
+        #endregion
+
+        #region Construction
+        public HardDrive(string driveLetter)
+        {
+             _driveInfo = new DriveInfo(driveLetter);
+        }
+        #endregion
+
+        #region Hard-drive attributes
+        public long GetTotalSize()
+        {
+            long result;
+
+            if (_driveInfo.IsReady)
+            {
+                result = _driveInfo.TotalSize;
+            }
+            else
+            {
+                throw new Exception($"Drive {_driveInfo.Name} is not ready.");
+            }
+
+            return result; 
+        }
+        public long GetTotalFreeSpace()
+        {
+            long result;
+
+            if (_driveInfo.IsReady)
+            {
+                result = _driveInfo.TotalFreeSpace;
+            }
+            else
+            {
+                throw new Exception($"Drive {_driveInfo.Name} is not ready.");
+            }
+
+            return result;
+        }
+        public string GetDriveFormat()
+        {
+            string result;
+
+            if (_driveInfo.IsReady)
+            {
+                result = _driveInfo.DriveFormat;
+            }
+            else
+            {
+                throw new Exception($"Drive {_driveInfo.Name} is not ready.");
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
