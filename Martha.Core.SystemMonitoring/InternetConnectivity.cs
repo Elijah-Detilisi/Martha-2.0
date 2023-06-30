75edc7a60662d0e580a4bdeeb5a8e39cd2d3b962
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-30 22:47:50 +0200

    Create InternetConnectivity.cs

---
+++ Martha.Core.SystemMonitoring/InternetConnectivity.cs
@@ -0,0 +1,27 @@
+﻿namespace Martha.Core.SystemMonitoring
+{
+    using System.Net.NetworkInformation;
+
+    public static class InternetConnectivity
+    {
+        public static bool IsInternetAvailable()
+        {
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    const string targetHost = "www.google.com";
+                    const int timeout = 3000; // 3 seconds
+
+                    PingReply reply = ping.Send(targetHost, timeout);
+                    return reply.Status == IPStatus.Success;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to check internet connectivity: {ex.Message}");
+                return false; // Assume internet is not available on exception
+            }
+        }
+    }
+}
